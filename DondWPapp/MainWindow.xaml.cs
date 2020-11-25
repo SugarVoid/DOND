3f2919637cf5301e34aced6aaf379329cedd8f0e
Devonte Durham <durhamd@fvtc.edu>
2020-11-25 04:58:12 -0600

    Added wpf

---
+++ DondWPapp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DondWPapp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            case1.Click += Button_Click;
+            case2.Click += Button_Click;
+            case3.Click += Button_Click;
+        }
+
+        private void SetUpButtons()
+        {
+            for (int i = 1; i <= 26; i++)
+            {
+                
+                
+            }
+        }
+
+
+        void Button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            // use Name or Tag of button
+            MessageBox.Show(button.Name);
+        }
+    }
+}
